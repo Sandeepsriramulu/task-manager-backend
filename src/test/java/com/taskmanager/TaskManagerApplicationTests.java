91bdf14dfe43b12d8cf6a51a395d6253354fe2ad
sandeep <mailmesandeep2004@gmail.com>
2026-09-01 15:55:11 +0000

    Day 39: Add Maven unit test

---
+++ src/test/java/com/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.taskmanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TaskManagerApplicationTests {
+
+    @Test
+    void testAddition() {
+        int result = 2 + 3;
+
+        assertEquals(5, result);
+    }
+}
