54458960e3ef9716cc21cbd6b42bc4e6aba61b6b
sandeep <mailmesandeep2004@gmail.com>
2026-09-02 11:50:58 +0000

    Add intentional failing test

---
+++ src/test/java/com/taskmanager/FailingTest.java
@@ -0,0 +1,13 @@
+package com.taskmanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FailingTest {
+
+    @Test
+    void thisWillFail() {
+        assertEquals(1, 2, "Intentional failure for Jenkins demo");
+    }
+}
